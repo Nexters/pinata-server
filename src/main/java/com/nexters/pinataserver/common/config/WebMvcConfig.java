3f8907f1654d2d4e1e5be897374b480fd797c859
devrunner21 <devrunner21@gmail.com>
2022-07-30 14:16:37 +0900

    chore: CORS 설정을 진행한다 (#15)

--- src/main/java/com/nexters/pinataserver/common/config/WebMvcConfig.java
+++ src/main/java/com/nexters/pinataserver/common/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,6 +31,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(authenticationArgumentResolver);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+				.allowedMethods("GET", "POST","PUT","DELETE","OPTIONS")
+				.maxAge(3000);
 	}
 
 }
