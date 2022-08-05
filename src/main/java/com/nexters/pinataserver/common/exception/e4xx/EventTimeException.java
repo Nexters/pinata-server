97e1e3b18bdf2f3fe1c1a35d49c221c72a9d8904
jihoon <jihoon@wemakeprice.com>
2022-08-05 15:48:45 +0900

    refactor: 에러코드를 정리한다 (#17)

--- src/main/java/com/nexters/pinataserver/common/exception/e4xx/EventTimeException.java
+++ src/main/java/com/nexters/pinataserver/common/exception/e4xx/EventTimeException.java
@@ -7,8 +7,8 @@ import com.nexters.pinataserver.common.exception.ResponseException;
 
 public enum EventTimeException implements ResponseDefinition {
 
-	TIME_OUT(HttpStatus.BAD_REQUEST, "ERR1002", "이벤트 참여 기간이 아닙니다."),
-	INVALID_INPUT(HttpStatus.BAD_REQUEST, "ERR1003", "이벤트 기간 입력이 잘못되었습니다.");
+	TIME_OUT(HttpStatus.BAD_REQUEST, "ERR1004", "이벤트 참여 기간이 아닙니다."),
+	INVALID_INPUT(HttpStatus.BAD_REQUEST, "ERR1005", "이벤트 기간 입력이 잘못되었습니다.");
 	private final ResponseException responseException;
 
 	EventTimeException(HttpStatus status, String code, String message) {
