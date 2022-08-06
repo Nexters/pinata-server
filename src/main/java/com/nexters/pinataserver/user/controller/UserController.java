d66fa2b6a5c6a1497e4dfb1dc33def20a5670abc
hyerin6 <hyerinn6@gmail.com>
2022-08-06 17:09:29 +0900

    feature: 유저 정보 조회 API 생성 - 필요 없는 파일 삭제 - User 정보 조회 API 구현 - issue #22

--- src/main/java/com/nexters/pinataserver/user/controller/UserController.java
+++ src/main/java/com/nexters/pinataserver/user/controller/UserController.java
@@ -5,16 +5,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nexters.pinataserver.common.dto.response.CommonApiResponse;
+import com.nexters.pinataserver.common.security.AuthenticationPrincipal;
+import com.nexters.pinataserver.user.domain.User;
+import com.nexters.pinataserver.user.dto.UserResponse;
+import com.nexters.pinataserver.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @RequestMapping("/api/v1")
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
+	private final UserService userService;
+
 	@GetMapping("/users/{id}")
-	public void getUser(@PathVariable("id") Long id) {
+	public CommonApiResponse<UserResponse> getUser(@PathVariable("id") Long id) {
+		User user = userService.getUserById(id);
+		return CommonApiResponse.ok(UserResponse.from(user));
 	}
 
 	@GetMapping("/me")
-	public void getMyInfo() {
+	public CommonApiResponse<UserResponse> getMyInfo(@AuthenticationPrincipal Long userId) {
+		User user = userService.getUserById(userId);
+		return CommonApiResponse.ok(UserResponse.from(user));
 	}
 
 }
