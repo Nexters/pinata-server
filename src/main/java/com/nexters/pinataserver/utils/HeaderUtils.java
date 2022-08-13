1f8f73db46bee47e0262ab550126ac977bfcfaa2
devrunner21 <devrunner21@gmail.com>
2022-08-13 19:53:23 +0900

    fix: 인증 요청 시 헤더 로그를 찍는다

--- src/main/java/com/nexters/pinataserver/utils/HeaderUtils.java
+++ src/main/java/com/nexters/pinataserver/utils/HeaderUtils.java
@@ -6,12 +6,18 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.WebRequest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class HeaderUtils {
 
 	public static final String BEARER_PREFIX = "Bearer ";
 
 	public static String getAccessToken(HttpServletRequest request) {
 		String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+		log.info("========================================");
+		log.info(request.getHeader(HttpHeaders.AUTHORIZATION));
+		log.info("========================================");
 
 		if (StringUtils.hasText(token)) {
 			return token.substring(BEARER_PREFIX.length());
@@ -22,6 +28,9 @@ public class HeaderUtils {
 
 	public static String getAccessToken(WebRequest request) {
 		String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+		log.info("========================================");
+		log.info(request.getHeader(HttpHeaders.AUTHORIZATION));
+		log.info("========================================");
 
 		if (StringUtils.hasText(token)) {
 			return token.substring(BEARER_PREFIX.length());
