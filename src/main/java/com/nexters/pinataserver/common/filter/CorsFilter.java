5484a58305ed1e9d3e0e17ff2ff0bf31eea08146
devrunner21 <devrunner21@gmail.com>
2022-08-13 20:13:41 +0900

    fix: CORS Filter를 추가한다

---
+++ src/main/java/com/nexters/pinataserver/common/filter/CorsFilter.java
@@ -0,0 +1,56 @@
+package com.nexters.pinataserver.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+		throws IOException, ServletException
+	{
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods","*");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+
+		log.info("======================================== CorsFilter");
+		log.info(request.getHeader(HttpHeaders.AUTHORIZATION));
+		log.info("======================================== CorsFilter");
+
+
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, res);
+		}
+	}
+
+	public void init(FilterConfig filterConfig) {
+		// not needed
+	}
+
+	public void destroy() {
+		//not needed
+	}
+}
